1500e13126db6192f3bcda159fb7193f00f4bf4f
sd <szdenes@icloud.com>
2022-01-10 14:23:24 +0100

    adding thumbnails

--- public/sw.js
+++ public/sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1af2af8e1c70c34388b1.js"
+    "url": "webpack-runtime-4bd62126269b7f21bfc2.js"
   },
   {
     "url": "framework-ffbe7db38cb78ec94d7b.js"
   },
   {
-    "url": "app-1f0f6e7ed4a9e556eee1.js"
+    "url": "app-92cf9b901dda6ad43b5b.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "36b998ffd5d33344cda9f6d718df623b"
+    "revision": "884a8ca0dcb23b3543bf8987b2c2eb44"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-16703ee5599528db9f93.js"
@@ -135,7 +135,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-1f0f6e7ed4a9e556eee1.js`))) {
+  if (!resources || !(await caches.match(`/app-92cf9b901dda6ad43b5b.js`))) {
     return await fetch(event.request)
   }
 
